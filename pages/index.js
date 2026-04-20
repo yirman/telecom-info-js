30da1c341fb10f9215c4a524466e6bedc976a0d4
yirman <germanguevara2496@gmail.com>
2026-04-20 15:47:21 -0400

    removing concepts titles

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,13 @@ export default function Home() {
         </section>
 
         <section className="definition">
-          <h2 className="definition-title">Definición</h2>
+          <h2 className="definition-title">Definición y conceptos fundamentales</h2>
           <div className="definition-inner">
             <p>El autor <strong>Roger L. Freeman</strong> define este concepto como la transmisión de información a distancia, la cual abarca no solo la voz, sino también datos e imágenes mediante señales eléctricas.</p>
           </div>
         </section>
 
         <section className="infographic">
-
-        <section className="concepts-header">
-          <h2 className="concepts-title">Conceptos fundamentales</h2>
-        </section>
         
           <div className="info-grid" role="list">
             <div className="card" role="listitem">
